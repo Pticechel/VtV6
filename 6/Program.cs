a31f600d2dc89993bccdca06cce143f484065ad3
Pticechel <70794998+Pticechel@users.noreply.github.com>
2023-03-11 16:29:52 +0300

    Add files via upload

---
+++ 6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int size = 10;
+        int num = 50;
+
+        Random rand = new Random();
+        List<int> list = Enumerable.Range(1, size)
+                                   .Select(x => rand.Next(100))
+                                   .Where(x => Math.Abs(x - num) <= 4)
+                                   .ToList();
+
+        Console.WriteLine(string.Join(", ", list));
+    }
+}
